0d37d2cdf3bef7811831689d73dfde68ce83da7b
Sarash Singh <sarashsingh019@gmail.com>
2026-07-03 22:30:09 +0530

    Fix email: switch to Brevo SMTP

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -5,7 +5,7 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: 'b0c763001@smtp-brevo.com',
+    user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
 });
